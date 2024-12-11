d3aa80440b74a73563c19d1ad91a23b1bd1bf54b
RohitSth <shrestha.rohit655@gmail.com>
2024-12-12 02:31:45 +0545

    update :: added cors in the main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ async function bootstrap() {
   // Retrieve the PORT from ConfigService (defaults to 3000 if not set)
   const port = 3000;
 
+  // -- Cors setup
+  app.enableCors({
+    origin: ['*', 'http://localhost:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(port);
   console.log(`Server is running on port http://localhost:${port}`);
 }
