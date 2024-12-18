56a2d3b788463037a787e29a8a318dd2e97e19e6
SatkarNiraula <satkarniraula69@gmail.com>
2024-12-18 15:13:14 +0545

    update :: make reusable entity

--- src/data-services/mgdb/entities/xplorer.entity.ts
+++ src/data-services/mgdb/entities/xplorer.entity.ts
@@ -1,23 +1,11 @@
 import { Column, Entity } from 'typeorm';
-import { BaseEntity } from './base.entity';
 import { UserTypeEnum } from 'src/common/enums/users/user-type.enum';
+import { BaseUserEntity } from './base.entity';
 
 @Entity('users')
-export class XplorerEntity extends BaseEntity {
-  @Column({ name: 'username', unique: true })
-  username: string;
-
-  @Column({ name: 'fullname' })
-  fullname: string;
-
+export class XplorerEntity extends BaseUserEntity {
   @Column({ name: 'user_type', default: UserTypeEnum.XPLORER })
   user_type: UserTypeEnum;
-
-  @Column({ name: 'email', unique: true })
-  email: string;
-
-  @Column({ name: 'password' })
-  password: string;
 
   @Column({ name: 'phone_number', unique: true })
   phone_number: string;
@@ -26,12 +14,5 @@ export class XplorerEntity extends BaseEntity {
     super();
     this.user_type = UserTypeEnum.XPLORER; // Explicitly set the default
     Object.assign(this, partial); // For optional parameter initialization
-  }
-
-  toJSON() {
-    return {
-      ...this,
-      password: undefined,
-    };
   }
 }
