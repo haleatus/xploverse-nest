724b57b5234fd0c8a7a828e4237edc54a97ea8fb
SatkarNiraula <satkarniraula69@gmail.com>
2024-12-18 12:27:04 +0545

    minor testing: auth guard module removed but working

--- src/application/guards/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthGuard } from './auth.guard';
-import { XplorerService } from 'src/application/features/users/xplorer/xplorer.service';
-
-@Module({
-  providers: [AuthGuard, XplorerService],
-  exports: [AuthGuard],
-})
-export class AuthGuardModule {}
