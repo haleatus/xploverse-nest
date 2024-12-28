e884c0eaeebe0ce627b75a9ddbb34532607560e0
SatkarNiraula <satkarniraula69@gmail.com>
2024-12-28 19:21:39 +0545

    update :: trip CRUD

--- src/application/guards/auth/auth.guard.ts
+++ src/application/guards/auth/auth.guard.ts
@@ -8,10 +8,16 @@ import {
 import { JwtTokenService } from 'src/libs/token/jwt/jwt-token.service';
 import { Request } from 'express';
 import { convertToObjectId } from 'src/common/utils/convert-to-object-id';
+import { UserUseCaseService } from 'src/use-cases/user-use-cases/user-use-case.service';
+import { AdminUseCaseService } from 'src/use-cases/admin-use-cases/admin-use-case.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtTokenService: JwtTokenService) {}
+  constructor(
+    private jwtTokenService: JwtTokenService,
+    private userUseCaseService: UserUseCaseService,
+    private adminUseCaseService: AdminUseCaseService,
+  ) {}
 
   private extractToken(request: Request): string | undefined {
     return request.headers.authorization?.split(' ')[1];
@@ -22,16 +28,28 @@ export class AuthGuard implements CanActivate {
     const { url: requestUrl } = request;
     const token = this.extractToken(request);
 
-    const isAdmin = requestUrl.startsWith('/api/admin') ? true : false;
-    const isUser = requestUrl.startsWith('/api/user') ? true : false;
-
     if (!token) {
       throw new UnauthorizedException('Invalid token');
     }
 
+    const isAdmin = requestUrl.startsWith('/api/admin') ? true : false;
+    const isUser = requestUrl.startsWith('/api/user') ? true : false;
+
     try {
       const decoded = await this.jwtTokenService.checkToken(token);
-      request.user = convertToObjectId(decoded._id);
+      if (isAdmin) {
+        const admin = await this.adminUseCaseService.findAdminById(
+          convertToObjectId(decoded._id),
+        );
+        request.admin = admin;
+      }
+
+      if (isUser) {
+        const user = await this.userUseCaseService.findUserById(
+          convertToObjectId(decoded._id),
+        );
+        request.user = user;
+      }
     } catch (error) {
       Logger.error(error.message);
       throw new UnauthorizedException('Invalid Token');
--- src/use-cases/trip-use-cases/trip-use-case.service.ts
+++ src/use-cases/trip-use-cases/trip-use-case.service.ts
@@ -1,9 +1,10 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { TripEntity } from 'src/data-services/mgdb/entities/trip.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { TripDto } from 'src/core/dtos/request/trip.dto';
+import { CreateTripDto, updateTripDto } from 'src/core/dtos/request/trip.dto';
 import { TripStatusEnum } from 'src/common/enums/trip-status.enum';
+import { convertToObjectId } from 'src/common/utils/convert-to-object-id';
 
 @Injectable()
 export class TripUseCaseService {
@@ -17,11 +18,41 @@ export class TripUseCaseService {
     return trips;
   }
 
-  async createTrip(dto: TripDto): Promise<TripEntity> {
+  async createTrip(dto: CreateTripDto): Promise<TripEntity> {
     const newTrip = this.tripRepository.create({
       ...dto,
       trip_status: dto.trip_status ?? TripStatusEnum.PENDING,
     });
     return await this.tripRepository.save(newTrip);
+  }
+
+  async findTripById(id: string) {
+    const trip_id = convertToObjectId(id);
+    const trip = this.tripRepository.findOneBy({ _id: trip_id });
+    if (!trip) {
+      throw new NotFoundException('Trip does not exist');
+    }
+    return trip;
+  }
+
+  async updateTrip(id: string, dto: updateTripDto) {
+    const trip_id = convertToObjectId(id);
+    const trip = await this.tripRepository.findOneBy({ _id: trip_id });
+    if (!trip) {
+      throw new NotFoundException('Trip does not exist');
+    }
+    const updatedTrip = { ...trip, ...dto };
+    await this.tripRepository.update({ _id: trip._id }, updatedTrip);
+    return updatedTrip;
+  }
+
+  async deleteTripById(id: string) {
+    const trip_id = convertToObjectId(id);
+    const deletedTrip = this.tripRepository.findOneBy({ _id: trip_id });
+    if (!deletedTrip) {
+      throw new NotFoundException('Trip does not exist');
+    }
+    await this.tripRepository.delete({ _id: trip_id });
+    return deletedTrip;
   }
 }
