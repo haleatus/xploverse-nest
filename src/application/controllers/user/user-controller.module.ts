f4cb6d2dc56eecd88b79c0b9ae7ee25cad36ed94
SatkarNiraula <satkarniraula69@gmail.com>
2024-12-30 20:42:57 +0545

    update :: minor changes

--- src/application/controllers/user/user-controller.module.ts
+++ src/application/controllers/user/user-controller.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserUseCaseModule } from 'src/use-cases/user-use-cases/user-use-case.module';
 import { UserController } from './user.controller';
 import { UserTripController } from './user-trip/user-trip.controller';
+import { UserCarPoolRequestController } from './user-carpool-request/user-carpool-request.controller';
 
 @Module({
   imports: [UserUseCaseModule],
-  controllers: [UserController, UserTripController],
+  controllers: [
+    UserController,
+    UserTripController,
+    UserCarPoolRequestController,
+  ],
 })
 export class UserControllerModule {}
