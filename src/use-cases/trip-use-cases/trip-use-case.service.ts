2d713b1e2e7d693674087a65af21e4918f279ceb
SatkarNiraula <satkarniraula69@gmail.com>
2025-01-01 11:40:53 +0545

    update :: planner with trip

--- src/use-cases/trip-use-cases/trip-use-case.service.ts
+++ src/use-cases/trip-use-cases/trip-use-case.service.ts
@@ -4,19 +4,32 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 
 import { convertToObjectId } from 'src/common/utils/convert-to-object-id';
+import { UserEntity } from 'src/data-services/mgdb/entities/user.entity';
 
 @Injectable()
 export class TripUseCaseService {
   constructor(
     @InjectRepository(TripEntity)
     private tripRepository: Repository<TripEntity>,
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
   ) {}
 
   async findAllTrip() {
     const trips = await this.tripRepository.find({
       where: { is_car_pool: true },
     });
-    return trips;
+
+    return await Promise.all(
+      trips.map(async (trip) => {
+        const planner = await this.userRepository.findOne({
+          where: { _id: trip.planner },
+          select: ['username', 'email', 'phone_number'],
+        });
+
+        return { ...trip, planner };
+      }),
+    );
   }
 
   async findTripById(id: string) {
