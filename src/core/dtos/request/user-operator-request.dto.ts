c58a6df26c18246e5e111e80426aa8ddb018e631
SatkarNiraula <satkarniraula69@gmail.com>
2025-01-20 21:19:30 +0545

    update :: admin user operator use cases

---
+++ src/core/dtos/request/user-operator-request.dto.ts
@@ -0,0 +1,5 @@
+import { UserOperatorRequestStatusEnum } from 'src/common/enums/user-operator-request-status.enum';
+
+export class UserOperatorRequestDto {
+  operator_request_status: UserOperatorRequestStatusEnum;
+}
--- src/use-cases/admin-use-cases/admin-use-case.module.ts
+++ src/use-cases/admin-use-cases/admin-use-case.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { CryptoModule } from 'src/libs/crypto/crypto.module';
 import { AdminUseCaseService } from './admin-use-case.service';
 import { AdminAuthUseCaseModule } from './admin-auth/admin-auth-use-case.module';
+import { AdminUserOperatorUseCaseModule } from './admin-user-operator/admin-user-operator-use-case.module';
 
 @Module({
-  imports: [CryptoModule, AdminAuthUseCaseModule],
+  imports: [
+    CryptoModule,
+    AdminAuthUseCaseModule,
+    AdminUserOperatorUseCaseModule,
+  ],
   providers: [AdminUseCaseService],
-  exports: [AdminUseCaseService, AdminAuthUseCaseModule],
+  exports: [
+    AdminUseCaseService,
+    AdminAuthUseCaseModule,
+    AdminUserOperatorUseCaseModule,
+  ],
 })
 export class AdminUseCaseModule {}
