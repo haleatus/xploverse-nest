e71674c4e0c26b72cce0e60c56bca2fd03e179d0
SatkarNiraula <satkarniraula69@gmail.com>
2025-01-24 15:22:12 +0545

    fix :: user oeprator request initialing logic fix

--- src/use-cases/user-use-cases/user-operator/user-operator-use-case.service.ts
+++ src/use-cases/user-use-cases/user-operator/user-operator-use-case.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ObjectId } from 'mongodb';
 import AppNotFoundException from 'src/application/exception/app-not-found.exception';
+import AppException from 'src/application/exception/app.exception';
 import { UserOperatorRequestStatusEnum } from 'src/common/enums/user-operator-request-status.enum';
 import { UserOperatorRequestEntity } from 'src/data-services/mgdb/entities/user-operator-request.entity';
 import { UserEntity } from 'src/data-services/mgdb/entities/user.entity';
@@ -23,6 +24,18 @@ export class UserOperatorUseCaseService {
     const user = await this.userRepository.findOneBy({ _id: userId });
 
     if (!user) throw new AppNotFoundException('User does not exist');
+
+    const existingRequest = await this.userOperatorRequestRepository.findOne({
+      where: { requester: user._id },
+    });
+
+    if (
+      existingRequest &&
+      existingRequest.operator_request_status !==
+        UserOperatorRequestStatusEnum.DECLINED
+    ) {
+      throw new AppException('you must wait before you request is declined');
+    }
 
     const newRequest = this.userOperatorRequestRepository.create({
       requester: userId,
