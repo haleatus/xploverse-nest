17058d8723b5c17c8e2186ab0afe53f95a49fa77
SatkarNiraula <satkarniraula69@gmail.com>
2025-01-25 11:35:50 +0545

    update :: make file entity for external image api and remove multer

--- src/data-services/mgdb/entities/file.entity.ts
+++ src/data-services/mgdb/entities/file.entity.ts
@@ -4,6 +4,9 @@ import { BaseEntity } from './base.entity';
 @Entity('file')
 export class FileEntity extends BaseEntity {
   @Column()
+  file_id: string;
+
+  @Column()
   name: string;
 
   @Column()
@@ -11,4 +14,7 @@ export class FileEntity extends BaseEntity {
 
   @Column()
   path: string;
+
+  @Column()
+  url: string;
 }
