f2bd31fbc782369381a9bee36e5d6f04259aeebc
SatkarNiraula <satkarniraula69@gmail.com>
2025-01-27 16:16:58 +0545

    update :: chat and chat-message entity addeed

--- src/data-services/mgdb/entities/chat-message.entity.ts
+++ src/data-services/mgdb/entities/chat-message.entity.ts
@@ -1,5 +1,18 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { ObjectId } from 'mongodb';
 
 @Entity('chat-messages')
-export class chatMessage extends BaseEntity {}
+export class chatMessage extends BaseEntity {
+  @Column()
+  sender: ObjectId; // will reference the user who sends a message
+
+  @Column()
+  content: string; // message contents
+
+  @Column()
+  attachments: { url: string; path: string }[];
+
+  @Column()
+  chat: ObjectId; // will reference the chat that this message belongs to
+}
--- src/data-services/mgdb/entities/chat.entity.ts
+++ src/data-services/mgdb/entities/chat.entity.ts
@@ -1,5 +1,21 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { ObjectId } from 'mongodb';
 
 @Entity('chats')
-export class ChatEntity extends BaseEntity {}
+export class ChatEntity extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column()
+  is_group_chat: boolean;
+
+  @Column()
+  lastMessage: ObjectId; // will reference chat-messages
+
+  @Column()
+  participants: { participant: ObjectId }[]; // will reference the participated user
+
+  @Column()
+  chat_admin: ObjectId; // will reference the user who created the group chat (in case of a group chat)
+}
